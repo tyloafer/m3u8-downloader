abab47c7903031221a83072dba3334670d339bd6
moer <moer.lxy@antfin.com>
2022-06-16 17:23:29 +0800

    feat: 增加代理/本地m3u8文件解析/本地文件合并的能力

--- m3u8-downloader.go
+++ m3u8-downloader.go
@@ -1,7 +1,7 @@
-//@author:llychao<lychao_vip@163.com>
-//@contributor: Junyi<me@junyi.pw>
-//@date:2020-02-18
-//@功能:golang m3u8 video Downloader
+// @author:llychao<lychao_vip@163.com>
+// @contributor: Junyi<me@junyi.pw>
+// @date:2020-02-18
+// @功能:golang m3u8 video Downloader
 package main
 
 import (
@@ -43,6 +43,11 @@ var (
 	cFlag   = flag.String("c", "", "自定义请求 cookie")
 	sFlag   = flag.Int("s", 0, "是否允许不安全的请求(默认为0)")
 	spFlag  = flag.String("sp", "", "文件保存路径(默认为当前路径)")
+	fFlag   = flag.String("f", "", "自定义m3u8文件")
+	hFlag   = flag.String("h", "", "自定义host，配合 -f 使用")
+	kFlag   = flag.String("k", "", "自定义m3u8的aes key解密文件，不指定则默认走m3u8文件定义的key")
+	mFlag   = flag.String("m", "", "合并指定文件夹下所有的ts文件片段")
+	pFlag   = flag.String("p", "", "指定socks/http代理")
 
 	logger *log.Logger
 	ro     = &grequests.RequestOptions{
@@ -55,6 +60,8 @@ var (
 			"Accept-Language": "zh-CN,zh;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5",
 		},
 	}
+
+	proxy *url.URL
 )
 
 // TsInfo 用于保存 ts 文件的下载地址和文件名
@@ -87,6 +94,23 @@ func Run() {
 	insecure := *sFlag
 	savePath := *spFlag
 
+	if *mFlag != "" {
+		Merge(*mFlag)
+		return
+	}
+
+	if (!strings.HasPrefix(m3u8Url, "http") || m3u8Url == "") && *fFlag == "" {
+		flag.Usage()
+		return
+	}
+
+	var err error
+	proxy, err = url.Parse(*pFlag)
+	if err != nil {
+		fmt.Println("代理地址解析失败：" + err.Error())
+		os.Exit(-1)
+	}
+
 	ro.Headers["Referer"] = getHost(m3u8Url, "apiv2")
 	if insecure != 0 {
 		ro.InsecureSkipVerify = true
@@ -95,49 +119,59 @@ func Run() {
 	if cookie != "" {
 		ro.Headers["Cookie"] = cookie
 	}
-	if !strings.HasPrefix(m3u8Url, "http") || m3u8Url == "" {
-		flag.Usage()
-		return
-	}
-	var download_dir string
+
+	var downloadDir string
 	pwd, _ := os.Getwd()
 	if savePath != "" {
 		pwd = savePath
 	}
-	//pwd = "/Users/chao/Desktop" //自定义地址
-	download_dir = filepath.Join(pwd, movieDir)
-	if isExist, _ := pathExists(download_dir); !isExist {
-		os.MkdirAll(download_dir, os.ModePerm)
+	// pwd = "/Users/chao/Desktop" //自定义地址
+	downloadDir = filepath.Join(pwd, movieDir)
+	if isExist, _ := pathExists(downloadDir); !isExist {
+		os.MkdirAll(downloadDir, os.ModePerm)
 	}
 	m3u8Host := getHost(m3u8Url, hostType)
 	m3u8Body := getM3u8Body(m3u8Url)
-	//m3u8Body := getFromFile()
-	ts_key := getM3u8Key(m3u8Host, m3u8Body)
-	if ts_key != "" {
-		fmt.Printf("待解密 ts 文件 key : %s \n", ts_key)
+	// m3u8Body := getFromFile()
+	tsKey := getM3u8Key(m3u8Host, m3u8Body)
+	if tsKey != "" {
+		fmt.Printf("待解密 ts 文件 key : %s \n", tsKey)
 	}
-	ts_list := getTsList(m3u8Host, m3u8Body)
-	fmt.Println("待下载 ts 文件数量:", len(ts_list))
+	tsList := getTsList(m3u8Host, m3u8Body)
+	fmt.Println("待下载 ts 文件数量:", len(tsList))
 	// 下载ts
-	downloader(ts_list, maxGoroutines, download_dir, ts_key)
-	if ok := checkTsDownDir(download_dir); !ok {
+	downloader(tsList, maxGoroutines, downloadDir, tsKey)
+	Merge(downloadDir)
+	fmt.Printf("\n[Success] 下载保存路径：%s | 共耗时: %6.2fs\n", downloadDir+".mp4", time.Now().Sub(now).Seconds())
+}
+
+func Merge(path string) {
+	if ok := checkTsDownDir(path); !ok {
 		fmt.Printf("\n[Failed] 请检查url地址有效性 \n")
 		return
 	}
 	switch runtime.GOOS {
 	case "windows":
-		win_merge_file(download_dir)
+		win_merge_file(path)
 	default:
-		unix_merge_file(download_dir)
+		unix_merge_file(path)
 	}
-	os.Rename(filepath.Join(download_dir, "merge.mp4"), download_dir+".mp4")
-	os.RemoveAll(download_dir)
+	os.Rename(filepath.Join(path, "merge.mp4"), path+".mp4")
+	os.RemoveAll(path)
 	DrawProgressBar("Merging", float32(1), PROGRESS_WIDTH, "merge.ts")
-	fmt.Printf("\n[Success] 下载保存路径：%s | 共耗时: %6.2fs\n", download_dir+".mp4", time.Now().Sub(now).Seconds())
 }
 
 // 获取m3u8地址的host
 func getHost(Url, ht string) (host string) {
+	// 解析本地m3u8文件时，由用户传入host
+	if *hFlag != "" {
+		return *hFlag
+	}
+
+	if Url == "" {
+		return ""
+	}
+
 	u, err := url.Parse(Url)
 	checkErr(err)
 	switch ht {
@@ -151,13 +185,31 @@ func getHost(Url, ht string) (host string) {
 
 // 获取m3u8地址的内容体
 func getM3u8Body(Url string) string {
+	// 如果指定m3u8本地文件，则直接解析本地文件
+	if *fFlag != "" {
+		return getM3u8BodyLocal()
+	}
 	r, err := grequests.Get(Url, ro)
 	checkErr(err)
 	return r.String()
 }
 
+// 获取本地m3u8文件的内容体
+func getM3u8BodyLocal() string {
+	byt, err := ioutil.ReadFile(*fFlag)
+	if err != nil {
+		fmt.Println("读取本地m3u8文件失败，err:" + err.Error())
+		os.Exit(-1)
+	}
+	return string(byt)
+}
+
 // 获取m3u8加密的密钥
 func getM3u8Key(host, html string) (key string) {
+	// 如果指定 m3u8 key 本地文件，则直接解析本地文件
+	if *kFlag != "" {
+		return getM3u8KeyLocal()
+	}
 	lines := strings.Split(html, "\n")
 	key = ""
 	for _, line := range lines {
@@ -178,13 +230,22 @@ func getM3u8Key(host, html string) (key string) {
 	return
 }
 
+func getM3u8KeyLocal() (key string) {
+	file, err := ioutil.ReadFile(*kFlag)
+	if err != nil {
+		fmt.Println("读取本地 m3u8 key 文件失败，err:" + err.Error())
+		os.Exit(-1)
+	}
+	return string(file)
+}
+
 func getTsList(host, body string) (tsList []TsInfo) {
 	lines := strings.Split(body, "\n")
 	index := 0
 	var ts TsInfo
 	for _, line := range lines {
 		if !strings.HasPrefix(line, "#") && line != "" {
-			//有可能出现的二级嵌套格式的m3u8,请自行转换！
+			// 有可能出现的二级嵌套格式的m3u8,请自行转换！
 			index++
 			if strings.HasPrefix(line, "http") {
 				ts = TsInfo{
@@ -214,22 +275,30 @@ func getFromFile() string {
 func downloadTsFile(ts TsInfo, download_dir, key string, retries int) {
 	defer func() {
 		if r := recover(); r != nil {
-			//fmt.Println("网络不稳定，正在进行断点持续下载")
+			// fmt.Println("网络不稳定，正在进行断点持续下载")
 			downloadTsFile(ts, download_dir, key, retries-1)
 		}
 	}()
 	curr_path := fmt.Sprintf("%s/%s", download_dir, ts.Name)
 	if isExist, _ := pathExists(curr_path); isExist {
-		//logger.Println("[warn] File: " + ts.Name + "already exist")
+		// logger.Println("[warn] File: " + ts.Name + "already exist")
 		return
 	}
+	// 增加代理
+	u, err := url.Parse(ts.Url)
+	if err != nil {
+		fmt.Println("解析 ts url +" + ts.Url + "失败")
+		os.Exit(-1)
+	}
+	ro.Proxies[u.Scheme] = proxy
+
 	res, err := grequests.Get(ts.Url, ro)
 	if err != nil || !res.Ok {
 		if retries > 0 {
 			downloadTsFile(ts, download_dir, key, retries-1)
 			return
 		} else {
-			//logger.Printf("[warn] File :%s", ts.Url)
+			// logger.Printf("[warn] File :%s", ts.Url)
 			return
 		}
 	}
@@ -242,13 +311,13 @@ func downloadTsFile(ts TsInfo, download_dir, key string, retries int) {
 		contentLen, _ = strconv.Atoi(contentLenStr)
 	}
 	if len(origData) == 0 || (contentLen > 0 && len(origData) < contentLen) || res.Error != nil {
-		//logger.Println("[warn] File: " + ts.Name + "res origData invalid or err：", res.Error)
+		// logger.Println("[warn] File: " + ts.Name + "res origData invalid or err：", res.Error)
 		downloadTsFile(ts, download_dir, key, retries-1)
 		return
 	}
 	// 解密出视频 ts 源文件
 	if key != "" {
-		//解密 ts 文件，算法：aes 128 cbc pack5
+		// 解密 ts 文件，算法：aes 128 cbc pack5
 		origData, err = AesDecrypt(origData, []byte(key))
 		if err != nil {
 			downloadTsFile(ts, download_dir, key, retries-1)
@@ -258,7 +327,7 @@ func downloadTsFile(ts TsInfo, download_dir, key string, retries int) {
 	// https://en.wikipedia.org/wiki/MPEG_transport_stream
 	// Some TS files do not start with SyncByte 0x47, they can not be played after merging,
 	// Need to remove the bytes before the SyncByte 0x47(71).
-	syncByte := uint8(71) //0x47
+	syncByte := uint8(71) // 0x47
 	bLen := len(origData)
 	for j := 0; j < bLen; j++ {
 		if origData[j] == syncByte {
@@ -271,9 +340,9 @@ func downloadTsFile(ts TsInfo, download_dir, key string, retries int) {
 
 // downloader m3u8 下载器
 func downloader(tsList []TsInfo, maxGoroutines int, downloadDir string, key string) {
-	retry := 5 //单个 ts 下载重试次数
+	retry := 5 // 单个 ts 下载重试次数
 	var wg sync.WaitGroup
-	limiter := make(chan struct{}, maxGoroutines) //chan struct 内存占用 0 bool 占用 1
+	limiter := make(chan struct{}, maxGoroutines) // chan struct 内存占用 0 bool 占用 1
 	tsLen := len(tsList)
 	downloadCount := 0
 	for _, ts := range tsList {
@@ -356,7 +425,7 @@ func win_merge_file(path string) {
 // unix 合并文件
 func unix_merge_file(path string) {
 	os.Chdir(path)
-	//cmd := `ls  *.ts |sort -t "\." -k 1 -n |awk '{print $0}' |xargs -n 1 -I {} bash -c "cat {} >> new.tmp"`
+	// cmd := `ls  *.ts |sort -t "\." -k 1 -n |awk '{print $0}' |xargs -n 1 -I {} bash -c "cat {} >> new.tmp"`
 	cmd := `cat *.ts >> merge.tmp`
 	execUnixShell(cmd)
 	execUnixShell("rm -rf *.ts")
